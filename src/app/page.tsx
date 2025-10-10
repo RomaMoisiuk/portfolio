acbae8dbc18cb827611b2fb83a0d525fd2757b6c
YoYPolkin <polovynkoilya@gmail.com>
2025-10-10 17:03:35 +0200

    Prettier + Tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Page() {
   return (
     <main>
-      <div>My Portfolio</div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Submit
+      </button>
     </main>
   );
 }
