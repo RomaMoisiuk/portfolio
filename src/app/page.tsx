8f9fc44170a3f5deb5d327c43ef9881f28951386
YoYPolkin <polovynkoilya@gmail.com>
2025-10-11 13:17:23 +0200

    Add navigation links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
 export default function Page() {
-  return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        Submit
-      </button>
-    </main>
-  );
+  return <div>Main page</div>;
 }
